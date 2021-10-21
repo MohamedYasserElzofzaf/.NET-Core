1827304a0e14bf340b4e76685d6c54d6b4fc4cd9
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-10-21 13:41:51 +0200

    Getting user input

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -6,7 +6,9 @@ namespace firstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
             Console.ReadLine();
         }
     }
