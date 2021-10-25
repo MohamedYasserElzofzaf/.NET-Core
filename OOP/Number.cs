e9b37024ce86ad0ac9deb300215b2e0cb97bf557
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-10-25 17:16:23 +0200

    OOP

---
+++ OOP/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Number
+    {
+        public int x1;
+        public int x2;
+
+        public void show()
+        {
+            Console.WriteLine("Show");
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n1 = new Number();
+            Console.WriteLine(n1.x1);
+            Console.ReadLine();
+        }
+    }
+}
